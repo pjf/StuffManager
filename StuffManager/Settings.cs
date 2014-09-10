40dad637f77491c856d0e4dd3d389c56b551219c
Wetmelon <pfguenette@gmail.com>
2014-09-10 17:59:46 -0400

    https fix, static settings/paths file

---
+++ StuffManager/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuffManager
+{
+    static class Settings
+    {
+        // The root address of KerbalStuff
+        public static String KS_ROOT = "https://beta.kerbalstuff.com";
+
+        //API String for KerbalStuff
+        public static String KS_API = KS_ROOT + "/api";
+
+        //String for getting a user via KS's API
+        public static String KS_USER = KS_API + "/user";
+
+        //String for getting a mod via KS's API
+        public static String KS_MOD = KS_API + "/mod";
+
+        //String to search for mods via KS's API
+        public static String KS_MOD_SEARCH = KS_API + "/search/mod?query=";
+
+        //String to search for users via KS's API
+        public static String KS_USER_SEARCH = KS_API + "/search/user?query=";
+    }
+}
